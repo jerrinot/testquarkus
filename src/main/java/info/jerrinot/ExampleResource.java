a712b79a03764af68d9743a951a2482740ac962b
Jaromir Hamala <jaromir.hamala@gmail.com>
2020-07-11 15:30:56 +0200

    wip

--- src/main/java/info/jerrinot/ExampleResource.java
+++ src/main/java/info/jerrinot/ExampleResource.java
@@ -1,24 +1,29 @@
 package info.jerrinot;
 
+import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
 @Path("/hello")
 public class ExampleResource {
 
-    private final HazelcastInstance hz;
-
-    public ExampleResource(HazelcastInstance hz) {
-        this.hz = hz;
-    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient();
+        try {
+            IMap<Long, UUID> map = hazelcastInstance.getMap("myMap");
+            map.set(System.currentTimeMillis(), UUID.randomUUID());
+        } finally {
+            hazelcastInstance.shutdown();
+        }
+        return "stored";
     }
 }
